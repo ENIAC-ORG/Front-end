fb40579b7f3d0ea33836e60b30e78b223d71572e
dehghan82 <108016220+dehghan82@users.noreply.github.com>
2025-01-27 18:37:22 +0330

    fix test

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://127.0.0.1:5173/', // Change this if your app runs on a different port
+    baseUrl: 'http://eniacgroup.ir', // Change this if your app runs on a different port
     supportFile: 'cypress/support/e2e.js',
   },
 });
